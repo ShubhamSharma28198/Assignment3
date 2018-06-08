f67d1c3fda27b3a13136ba4180a683bfeee6690c
ShubhamSharma28198 <39906233+ShubhamSharma28198@users.noreply.github.com>
2018-06-08 14:23:09 +0530

    Add files via upload

---
+++ Questions Dt.py
@@ -0,0 +1,83 @@
+print("Ques 1.)  Input two integer number and practice following : ")
+print(80*'-') # Repeation of '-' 100 times.
+print("Ques 1.)  Input two integer number and practice following : ")
+print(80*'-') # Repeation of '-' 100 times.
+
+a=int(input("Enter 1st integer Number : "))
+b=int(input("Enter 2nd intger Number : "))
+
+print("add : ",a+b)
+print("sub : ",a-b)
+print("mul : ",a*b)
+print("power",a**b)
+print("power using diff method : ",pow(a,b))
+print("rem : ",a%b)
+print("div with float value : ",a/b)
+print("div with whole number : ",a//b)
+
+print(80*'-') # Repeation of '-' 100 times.
+
+
+
+
+
+print("Ques 2.)  Input two float number and practice following : ")
+print(80*'-') # Repeation of '-' 100 times.
+
+a=float(input("Enter 1st float Number : "))
+b=float(input("Enter 2nd float Number : "))
+
+print("add : ",a+b)
+print("sub : ",a-b)
+print("mul : ",a*b)
+print("power",a**b)
+print("power using diff method : ",pow(a,b))
+print("rem : ",a%b)
+print("div with float value : ",a/b)
+print("div with whole number : ",a//b)
+
+print(80*'-') # Repeation of '-' 100 times.
+
+
+
+print("Ques 3.)  Input two complex number and practice following : ")
+print(80*'-') # Repeation of '-' 100 times.
+a=complex(input("Enter complex Number : "))
+
+print("add : ",a+3)
+print("sub : ",a-2.2)
+print("mul : ",a*2)
+print("power",a**3)
+print("power using diff method : ",pow(a,3))
+print("div with float value : ",a/2)
+
+
+
+
+print("Ques 4.) input a string and apply some properties")
+print(80*'-') # Repeation of '-' 100 times.
+
+
+a=input("Enter the string : ")
+print(3*a)
+print(len(a))
+print(a[6:10])
+print(a.upper())
+print(a.lower())
+print(a.upper(),a.lower())
+print(80*'-') # Repeation of '-' 100 times.
+
+
+print(80*'-') # Repeation of '-' 100 times.
+print("Ques 5.) assign any int value and convert it into str")
+print(80*'-') # Repeation of '-' 100 times.
+
+x=(input("Enter any intger value to convert it into string : "))
+
+print(str(x))
+print(type(x))
+
+print(80*'-') # Repeation of '-' 100 times.
+
+
+
